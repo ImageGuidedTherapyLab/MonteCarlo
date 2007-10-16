d4b4f57a6aea0b63d8c4fda39c602b03ecea26ce
fuentes <fuentes@abaf0659-affb-4052-b7bf-27aea6606fda>
2007-10-16 17:10:32 +0000

    dimension errors dim1 is the z-axis and dim2 is the r-axis :(
    git-svn-id: https://svn.ices.utexas.edu/repos/dddas/codes/branches/montecarlo@1409 abaf0659-affb-4052-b7bf-27aea6606fda

--- ascii_write.m
+++ ascii_write.m
@@ -28,20 +28,21 @@ end
 
 fclose(fid);
 
+% TRANSPOSING THE MATRIX!!!!!!!!!!!!!!!!!!!!!!
 fid = fopen( sprintf('%s.fld',filebase) ,'w')
 % create rectilinear coordinate buffer
 bounds = zeros(size(A,1)+size(A,2),1);
-for i=0:size(A,1)-1
-    bounds(i+1) = i*delta_r;
-end
 for i=0:size(A,2)-1
-    bounds(size(A,1)+i+1) = i*delta_z;
+    bounds(i+1) = i*delta_r * .01; % meters
+end
+for i=0:size(A,1)-1
+    bounds(size(A,2)+i+1) = i*delta_z * .01; % meters
 end
 % write out avs file header
 fprintf(fid,'# AVS field file \n');
 fprintf(fid,'ndim=2 # number of dimensions in the field \n');
-fprintf(fid,'dim1= %d  # dimension of axis 1 \n',size(A,1));
-fprintf(fid,'dim2= %d  # dimension of axis 2 \n',size(A,2));
+fprintf(fid,'dim1= %d  # dimension of r-axis \n',size(A,2));
+fprintf(fid,'dim2= %d  # dimension of z-axis \n',size(A,1));
 fprintf(fid,'nspace=2        # number of physical coordinates per point\n');
 fprintf(fid,'veclen=2        # number of components at each point\n');
 fprintf(fid,'label =monte_carlo  # data labels \n');
--- multi_run.m
+++ multi_run.m
@@ -13,11 +13,11 @@ clear all
 % set global variables
 global BEAM_LOC   DIFF_LEN   DELTA_R   DELTA_Z 
 
-DIFF_LEN = 1;    % length of diffusing tip [cm]
-DELTA_R = .01;   %  DELTA_R  = spacing along the r-axis [cm]
-DELTA_Z = .01;   %  DELTA_Z  = spacing along the z-axis [cm]
-dim1 = 500; % grid dimensions
-dim2 = 501; % grid dimensions
+DIFF_LEN = .5;    % length of diffusing tip [cm]
+DELTA_R = .0117;   %  DELTA_R  = spacing along the r-axis [cm]
+DELTA_Z = .0117;   %  DELTA_Z  = spacing along the z-axis [cm]
+dimz = 257; % grid dimensions
+dimr = 128; % grid dimensions
 
 % beam radius
 R_0 = .38 ;  % [cm]
@@ -43,17 +43,17 @@ elseif(BeamType == 3)
 elseif(BeamType == 4)
    functer = @diff_init  ;
    namebase = 'Diff_g%d_mua%d_mus%d';
-   BEAM_LOC = 2.5;  % location in z-axis of center of diffusing tip [cm]
+   BEAM_LOC = 1.5;  % location in z-axis of center of diffusing tip [cm]
 end
 
 
 % create power grid
-P = ones(dim1,dim2);
-c = floor(BEAM_LOC/DELTA_Z)+1; %grid location of the center of the diff tip
-l = floor(.5*DIFF_LEN/DELTA_Z); %number of grid points from center of diff tip to end
-w = floor(.4/DELTA_R);   %number of grid points out from laser for cooling
-P(c-l:c+l,1:1+w) = 0;
-P = ones(dim1,dim2); % comment/uncomment for uniform power
+P = ones(dimz,dimr);
+%c = floor(BEAM_LOC/DELTA_Z)+1; %grid location of the center of the diff tip
+%l = floor(.5*DIFF_LEN/DELTA_Z); %number of grid points from center of diff tip to end
+%w = floor(.4/DELTA_R);   %number of grid points out from laser for cooling
+%P(c-l:c+l,1:1+w) = 0;
+P = ones(dimz,dimr); % comment/uncomment for uniform power
 
 
 % setup for parameter study 
@@ -64,15 +64,25 @@ g = [0,.1,.2,.3,.4,.5,.6,.7,.8,.9,.99];
 % set number of times to run
 nrun = 1 
 
-% Create baseline grid
-heating4 = zeros(dim1,dim2-1);
 
+% Create distance grid for isotropic comparison
+dist = zeros(dimz,dimr-1);
+for iii = 1:dimz
+    for jjj = 1:dimr-1
+      % should be units of meters
+      dist(iii,jjj) = .01* sqrt(  (            (jjj-.5)*DELTA_R ) ^ 2 ...
+                                + ( BEAM_LOC - (iii-.5)*DELTA_Z ) ^ 2     ) ;
+    end
+end
+ 
 %for ig = 1:size(g,1)
 %  for ia = 1:size(mu_a,1)
 %    for is = 1:size(mu_s,1)
 for ig = 8:8
   for ia = 1:1
     for is = 15:15
+      % initialize grid
+      heating4 = zeros(dimz,dimr-1);
       for i = 1:nrun
         i
         [R,A,T,grid,fluence,HEATING] = ...
@@ -81,13 +91,11 @@ for ig = 8:8
       end
       heating4 = heating4./nrun;  
       % calculate isotropic source term for comparison
-      [R,Z] = meshgrid(.01*[DELTA_R/2:DELTA_R:(dim1+0.5)*DELTA_R],...
-                       .01*[DELTA_Z/2:DELTA_Z:(dim2-1.5)*DELTA_Z]);  % meters
-      dist = sqrt(R.^2 + (BEAM_LOC*.01 - Z).^2);  % meters
       mu_tr  = (mu_a(ia) *100)+ (mu_s(is)*100) * (1-g(ig)); % 1/meters
       mu_eff = sqrt(3*(mu_a(ia)*100)*mu_tr); % 1/meters
       %isotropic should be units of  [W/m^3]
-      isotropic = 3/4*(mu_a(ia)*100)*mu_tr*pi*(P.*exp(-mu_eff*dist)).* dist.^-1;
+      isotropic = 3/4*(mu_a(ia)*100)*mu_tr*pi*...
+                      (P(:,1:dimr-1).*exp(-mu_eff*dist)).* dist.^-1;
       name = sprintf(namebase,ig,ia,is);
       ascii_write(heating4,isotropic,name,DELTA_R,DELTA_Z)
     end
