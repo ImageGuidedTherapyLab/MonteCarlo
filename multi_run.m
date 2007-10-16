8a77597405a7c3c059c04078cfa7f88970896092
fuentes <fuentes@abaf0659-affb-4052-b7bf-27aea6606fda>
2007-10-16 15:33:25 +0000

    units were wrong
    git-svn-id: https://svn.ices.utexas.edu/repos/dddas/codes/branches/montecarlo@1408 abaf0659-affb-4052-b7bf-27aea6606fda

--- multi_run.m
+++ multi_run.m
@@ -1,23 +1,53 @@
 close all
 clear all
 
+% [usage]
+% 
+%    set BeamType  and spacing parameters and then type:
+% 
+%            multi_run for matlab command prompt
+% 
+% 
+% 
+% 
 % set global variables
 global BEAM_LOC   DIFF_LEN   DELTA_R   DELTA_Z 
 
-BEAM_LOC = 2.5;  % location in z-axis of center of diffusing tip [cm]
 DIFF_LEN = 1;    % length of diffusing tip [cm]
 DELTA_R = .01;   %  DELTA_R  = spacing along the r-axis [cm]
 DELTA_Z = .01;   %  DELTA_Z  = spacing along the z-axis [cm]
-
+dim1 = 500; % grid dimensions
+dim2 = 501; % grid dimensions
 
 % beam radius
-
 R_0 = .38 ;  % [cm]
+
+% set function pointer representing type of beam
+%                    diff_init : interstitial beam
+%                    flat_init : Flat beam of radium R_0
+%                    gauss_init: Gaussian beam with 1/exp(2) radius R_0
+%                    point_init: Point source at the origin
+BeamType = 4;
+if(BeamType == 1)
+   functer = @gauss_init ;
+   namebase = 'Gauss_g%d_mua%d_mus%d';
+   BEAM_LOC = 0.0;  % location in z-axis of center of diffusing tip [cm]
+elseif(BeamType == 2)
+   functer = @flat_init  ;
+   namebase = 'Flat_g%d_mua%d_mus%d';
+   BEAM_LOC = 0.0;  % location in z-axis of center of diffusing tip [cm]
+elseif(BeamType == 3)
+   functer = @point_init ;
+   namebase = 'Point_g%d_mua%d_mus%d';
+   BEAM_LOC = 0.0;  % location in z-axis of center of diffusing tip [cm]
+elseif(BeamType == 4)
+   functer = @diff_init  ;
+   namebase = 'Diff_g%d_mua%d_mus%d';
+   BEAM_LOC = 2.5;  % location in z-axis of center of diffusing tip [cm]
+end
 
 
 % create power grid
-dim1 = 500;
-dim2 = 501;
 P = ones(dim1,dim2);
 c = floor(BEAM_LOC/DELTA_Z)+1; %grid location of the center of the diff tip
 l = floor(.5*DIFF_LEN/DELTA_Z); %number of grid points from center of diff tip to end
@@ -25,16 +55,6 @@ w = floor(.4/DELTA_R);   %number of grid points out from laser for cooling
 P(c-l:c+l,1:1+w) = 0;
 P = ones(dim1,dim2); % comment/uncomment for uniform power
 
-
-% set function pointer representing type of beam
-%                    diff_init : interstitial beam
-%                    flat_init : Flat beam of radium R_0
-%                    gauss_init: Gaussian beam with 1/exp(2) radius R_0
-%                    point_init: Point source at the origin
-functer = @gauss_init ;
-functer = @flat_init  ;
-functer = @point_init ;
-functer = @diff_init  ;
 
 % setup for parameter study 
 mu_a = [.44,.704,1.008,1.312,1.616,1.92,2.224,2.528,2.832,3.136,3.44,3.744,4.048,4.352,4.656,4.96,5.0];
@@ -47,8 +67,6 @@ nrun = 1
 % Create baseline grid
 heating4 = zeros(dim1,dim2-1);
 
-namebase = 'Flat_g%d_mua%d_mus%d';
-namebase = 'Gauss_g%d_mua%d_mus%d';
 %for ig = 1:size(g,1)
 %  for ia = 1:size(mu_a,1)
 %    for is = 1:size(mu_s,1)
@@ -63,12 +81,13 @@ for ig = 8:8
       end
       heating4 = heating4./nrun;  
       % calculate isotropic source term for comparison
-      [R,Z] = meshgrid([DELTA_R/2:DELTA_R:(dim1+0.5)*DELTA_R],...
-                       [DELTA_Z/2:DELTA_Z:(dim2-1.5)*DELTA_Z]);
-      dist = sqrt(R.^2 + (BEAM_LOC - Z).^2);
-      mu_tr  = mu_a(ia) + mu_s(is) * (1-g(ig));
-      mu_eff = sqrt(3*mu_a(ia)*mu_tr);
-      isotropic = 3/4*mu_a(ia)*mu_tr*pi* (P.*exp(-mu_eff*dist)).* dist.^-1;
+      [R,Z] = meshgrid(.01*[DELTA_R/2:DELTA_R:(dim1+0.5)*DELTA_R],...
+                       .01*[DELTA_Z/2:DELTA_Z:(dim2-1.5)*DELTA_Z]);  % meters
+      dist = sqrt(R.^2 + (BEAM_LOC*.01 - Z).^2);  % meters
+      mu_tr  = (mu_a(ia) *100)+ (mu_s(is)*100) * (1-g(ig)); % 1/meters
+      mu_eff = sqrt(3*(mu_a(ia)*100)*mu_tr); % 1/meters
+      %isotropic should be units of  [W/m^3]
+      isotropic = 3/4*(mu_a(ia)*100)*mu_tr*pi*(P.*exp(-mu_eff*dist)).* dist.^-1;
       name = sprintf(namebase,ig,ia,is);
       ascii_write(heating4,isotropic,name,DELTA_R,DELTA_Z)
     end
