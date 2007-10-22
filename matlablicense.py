a94918aa2f1cc0c304348972186966b43548ac4c
fuentes <fuentes@abaf0659-affb-4052-b7bf-27aea6606fda>
2007-10-22 00:31:58 +0000

    to run matlab w/ screen and log out matlab must be run w/o X windows
    git-svn-id: https://svn.ices.utexas.edu/repos/dddas/codes/branches/montecarlo@1439 abaf0659-affb-4052-b7bf-27aea6606fda

--- matlablicense.py
+++ matlablicense.py
@@ -1,6 +1,6 @@
 import os
 from time import sleep
 
-while(os.system("matlab")):
+while(os.system("matlab -nodisplay")):
    print "waiting for license..."
    sleep(1)
--- multi_run.m
+++ multi_run.m
@@ -203,36 +203,36 @@ end
 
 ntotal = size(g,1)*size(mu_a,1)*size(mu_s,1)*size(dist_r,1)*size(dist_z,1);
 icount = 0;  
-for ig = 1:size(g,1)
+for is = 1:size(mu_s,1)
   for ia = 1:size(mu_a,1)
-    for is = 1:size(mu_s,1)
-       for ir = 1:size(dist_r,1)
-          for iz = 1:size(dist_z,1)
-             disp(sprintf('\non grid %d of %d total grids',icount,ntotal))
-             disp(sprintf('g = %f, mu_a = %f, mu_s = %f',...
-                                                    g(ig),mu_a(ia),mu_s(is) ) )
-             disp(sprintf('dist_r = %f, dist_z = %f', dist_r(ir),dist_z(iz) ) )
-             % calculate isotropic source term for comparison
-             mu_tr  = (mu_a(ia) *100)+ (mu_s(is)*100) * (1-g(ig)); % 1/meters
-             mu_eff = sqrt(3*(mu_a(ia)*100)*mu_tr); % 1/meters
-             %isotropic should be units of  [W/m^3]
-             isotropic = 3/4*(mu_a(ia)*100)*mu_tr*pi*...
-                             (P(:,1:dimr-1).*exp(-mu_eff*dist)).* dist.^-1;
-             %initialize monte carlo grid
-             heating4 = zeros(dimz,dimr-1);
-             for i = 1:nrun
-               disp(sprintf('  photon set %d of %d',i,nrun))
-               [R,A,T,grid,fluence,HEATING] = ...
-               scatter_simulation(10000,R_0,g(ig),mu_a(ia),mu_s(is),P,...
-                                  functer,dist_r(ir),dist_z(iz));
-               heating4 = heating4 + HEATING;
-             end
-             %monte carlo heating should be units of  [W/m^3]
-             heating4 = heating4./nrun;  
-             %output results
-             name = sprintf(namebase,icount); icount = icount + 1 ;
-             ascii_write(heating4,isotropic,name,DELTA_R,DELTA_Z,...
-                         g(ig),mu_a(ia),mu_s(is),dist_r(ir),dist_z(iz));
+    for ig = 1:size(g,1)
+      for ir = 1:size(dist_r,1)
+        for iz = 1:size(dist_z,1)
+           disp(sprintf('\non grid %d of %d total grids',icount,ntotal))
+           disp(sprintf('g = %f, mu_a = %f, mu_s = %f',...
+                                                  g(ig),mu_a(ia),mu_s(is) ) )
+           disp(sprintf('dist_r = %f, dist_z = %f', dist_r(ir),dist_z(iz) ) )
+           % calculate isotropic source term for comparison
+           mu_tr  = (mu_a(ia) *100)+ (mu_s(is)*100) * (1-g(ig)); % 1/meters
+           mu_eff = sqrt(3*(mu_a(ia)*100)*mu_tr); % 1/meters
+           %isotropic should be units of  [W/m^3]
+           isotropic = 3/4*(mu_a(ia)*100)*mu_tr*pi*...
+                           (P(:,1:dimr-1).*exp(-mu_eff*dist)).* dist.^-1;
+           %initialize monte carlo grid
+           heating4 = zeros(dimz,dimr-1);
+           for i = 1:nrun
+             disp(sprintf('  photon set %d of %d',i,nrun))
+             [R,A,T,grid,fluence,HEATING] = ...
+             scatter_simulation(10000,R_0,g(ig),mu_a(ia),mu_s(is),P,...
+                                functer,dist_r(ir),dist_z(iz));
+             heating4 = heating4 + HEATING;
+           end
+           %monte carlo heating should be units of  [W/m^3]
+           heating4 = heating4./nrun;  
+           %output results
+           name = sprintf(namebase,icount); icount = icount + 1 ;
+           ascii_write(heating4,isotropic,name,DELTA_R,DELTA_Z,...
+                       g(ig),mu_a(ia),mu_s(is),dist_r(ir),dist_z(iz));
          end
        end
      end
